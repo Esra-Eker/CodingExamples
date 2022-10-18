fcf906b0ba4578fe061702c985c62d30d80f0d91
Esra <elifesraeker@gmail.com>
2022-10-18 11:31:43 +0300

    Pangrams problem

---
+++ Pangrams/Pangrams/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Pangrams
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+
+            string result = pangrams(s);
+            Console.WriteLine(result);
+        }
+        public static string pangrams(string s)
+        {
+            for(int i = 97; i <= 122; i++)
+            {
+                if(!s.ToLower().Contains((char)i))
+                {
+                    return "not pangram";
+                }
+            }
+            return  "pangram";
+        }
+    }
+}
