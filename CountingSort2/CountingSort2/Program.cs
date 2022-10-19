9fc3dd281462c1bc75499d10f5c28e84b2a6d41a
Esra <elifesraeker@gmail.com>
2022-10-19 17:36:32 +0300

    Counting Sort 2 problem

---
+++ CountingSort2/CountingSort2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CountingSort2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp))
+                .ToList();
+
+            countingSort(arr, n);
+        }
+
+        public static void countingSort(List<int> arr, int n)
+        {
+            int[] dizi = new int[arr.Max() + 1];
+
+            for (int i = 0; i < arr.Count; i++)
+            {
+                dizi[arr[i]]++;
+            }
+
+            for (int i = 0, j = 0; i < dizi.Length; i++)
+            {
+                while (dizi[i] > 0)
+                {
+                    arr[j] = i;
+                    j++;
+                    dizi[i]--;
+                }
+            }
+
+            foreach (var item in arr)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+}
