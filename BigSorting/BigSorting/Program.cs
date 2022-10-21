8d3b475c1df3b7f89757dd199bc64c7da460878c
Esra <elifesraeker@gmail.com>
2022-10-21 09:37:33 +0300

    Big Sorting problem

---
+++ BigSorting/BigSorting/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BigSorting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<string> unsorted = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string unsortedItem = Console.ReadLine();
+                unsorted.Add(unsortedItem);
+            }
+
+            List<string> result = bigSorting(unsorted);
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public static List<string> bigSorting(List<string> unsorted)
+        {
+            var result = unsorted.OrderBy(a=>a.Length).ThenBy(a=>a).ToList();
+            return result;
+        }
+    }
+}
