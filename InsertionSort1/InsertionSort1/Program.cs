cd436625eb0cc5153cc0f0f3153fe88daec2276d
Esra <elifesraeker@gmail.com>
2022-10-25 11:04:11 +0300

    Insertion Sort 1 problem

---
+++ InsertionSort1/InsertionSort1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace InsertionSort1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            insertionSort1(n, arr);
+        }
+        public static void insertionSort1(int n, List<int> arr)
+        {
+            int lastNum = arr[arr.Count - 1];
+            //arr[arr.Count - 1] = arr[arr.Count - 2];
+
+            for (int i = arr.Count - 2; 0 <= i; i--)
+            {
+                if (arr[i] > lastNum)
+                {
+                    arr[i + 1] = arr[i];
+                    foreach (var item in arr)
+                        Console.Write(item + " ");
+                    Console.WriteLine();
+                    if (i == 0)
+                    {
+                        arr[i] = lastNum;
+                        foreach (var item in arr)
+                            Console.Write(item + " ");
+                        Console.WriteLine();
+                    }
+                }
+                else
+                {
+                    arr[i + 1] = lastNum;
+                    foreach (var item in arr)
+                        Console.Write(item + " ");
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
